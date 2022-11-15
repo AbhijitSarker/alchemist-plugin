52c8368cbf313f67bd9b74fd3806a43ebb98785e
Abhijitsarker <abhijitsarker03@gmail.com>
2022-11-16 04:43:02 +0600

    first commit

---
+++ alchemist-plugin.php
@@ -0,0 +1,33 @@
+<?php
+
+
+/**
+ * Plugin Name: Alchemist Plugin
+ * Plugin URI: https://abhijitsarker.github.io/my-portfolio/
+ * Description: This is my second time developing a plugin.
+ * Version: 1.0
+ * Requires at least: 5.2
+ * Requires PHP: 7.2
+ * Author: Abhijit Sarker
+ * Author URI: https://abhijitsarker.github.io/my-portfolio/
+ * License: GPL v2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI: https://example.com/my-plugin/
+ * Text Domain: alchemist
+ * Domain Path: /languages
+ */
+
+ /*
+{Alchemist} is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+
+{Alchemist} is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with {Alchemist}. If not, see {URI to Plugin License}.
+*/
