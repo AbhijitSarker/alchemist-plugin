50d989e88e51ac9341f622e32d2e1d084c3653e6
Abhijitsarker <abhijitsarker03@gmail.com>
2022-11-22 10:56:43 +0600

    Admin Custom fields

--- inc/Api/SettingsApi.php
+++ inc/Api/SettingsApi.php
@@ -8,6 +8,12 @@ class SettingsApi
 
     public $admin_subpages = array();
 
+    public $settings = array();
+
+    public $sections = array();
+
+    public $fields = array();
+
 
     public function register()
     {
@@ -76,6 +82,62 @@ class SettingsApi
                 $page['capability'],
                 $page['menu_slug'],
                 $page['callback']
+            );
+        }
+    }
+
+    public function addSettings(array $settings)
+    {
+        $this->settings = $settings;
+
+        return $this;
+    }
+
+    public function addSections(array $sections)
+    {
+        $this->sections = $sections;
+
+        return $this;
+    }
+
+    public function addFields(array $fields)
+    {
+        $this->fields = $fields;
+
+        return $this;
+    }
+
+    public function registerCustomFields()
+    {
+        //register settings
+        foreach ($this->settings as $setting) {
+            register_setting(
+                $setting["option_group"],
+                $setting["option_name"],
+                (isset($setting["callback"]) ? $setting["callback"] : '')
+            );
+        }
+
+        //add settings section
+        foreach ($this->sections as $section) {
+            add_settings_section(
+                $section["id"],
+                $section["title"],
+                (isset($section["callback"]) ? $section["callback"] : ''),
+                $section["page"]
+            );
+        }
+
+        //add settings 
+        foreach ($this->fields as $field) {
+            add_settings_field(
+                $field['id'],
+                $field['title'],
+                (isset($field["callback"]) ? $field["callback"] : ''),
+                $field['page'],
+                $field['section'],
+                (isset($field["args"]) ? $field["args"] : ''),
+
             );
         }
     }
